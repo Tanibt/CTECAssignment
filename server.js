dc23703213f7e08cbfa60c4e6ceba2c135dc4ba5
Tanicr7 <65279774+Tanicr7@users.noreply.github.com>
2022-08-17 21:32:13 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require("express"); //using the express web framework
-
+const port = 3000;
 var musicController = require('./controllers/musicController');
 var commentController = require('./controllers/commentController');
 var profileController = require('./controllers/profileController');
@@ -23,6 +23,6 @@ app.route('/profile').delete(profileController.deleteProfile);
 app.route('/login').post(profileController.loginProfile);
 app.route('/user').post(profileController.getProfile);
 
-
-app.listen(8080, "127.0.0.1"); // start the nodejs to be listening for incoming request @ port 8080
-console.log("web server running @ http://127.0.0.1:8080"); // output to console 
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+//app.listen(8080, "127.0.0.1"); // start the nodejs to be listening for incoming request @ port 8080
+//console.log("web server running @ http://127.0.0.1:8080"); // output to console 
