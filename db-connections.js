4b0e48a719a4821ab74594afebd82fabf98beaa0
Tanicr7 <65279774+Tanicr7@users.noreply.github.com>
2022-08-17 21:40:20 +0800

    Update db-connections.js

--- db-connections.js
+++ db-connections.js
@@ -1,14 +1,14 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host:'localhost',
+    host:'musicityctec.cegt9mmyyexq.us-east-1.rds.amazonaws.com',
     port: '3306',
-    user:'root',
-    password:'cdev',
+    user:'admin',
+    password:'musicity',
     database: 'musicity'
 });
 
 connection.connect(err => {
     if (err) throw err;
-    console.log('Connected To DB');
+    console.log('Connected To AWS DB');
 });
 module.exports = connection;
