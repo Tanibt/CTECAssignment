0acaf02c07ebcde5838ccd5e4c8a8adb2747772c
Tanicr7 <65279774+Tanicr7@users.noreply.github.com>
2022-08-19 11:03:31 +0800

    Update updateProfile.js

--- public/js/updateProfile.js
+++ public/js/updateProfile.js
@@ -2,7 +2,7 @@ function update(){
 
     var updateUser = new XMLHttpRequest();
 
-    updateUser.open("PUT","http://127.0.0.1:8080/profile", true)
+    updateUser.open("PUT","/profile", true)
     updateUser.setRequestHeader("Content-Type","application/json");
     updateUser.onload = function() {
         $('#successModal').modal('show');
@@ -22,7 +22,7 @@ function deleteProfile(){
 
     var deleteUser = new XMLHttpRequest();
 
-    deleteUser.open("DELETE","http://127.0.0.1:8080/profile", true)
+    deleteUser.open("DELETE","/deleteProfile", true)
     deleteUser.setRequestHeader("Content-Type","application/json");
     deleteUser.onload = function() {
         window.location.href="home.html";
